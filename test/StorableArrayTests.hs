2eeb43a71489a7efd4959d390e03d00722900be8
Dmitry Astapov <dastapov@gmail.com>
2010-11-10 11:25:06 +0200

    Fixed 2 warnings in tests

--- test/StorableArrayTests.hs
+++ test/StorableArrayTests.hs
@@ -10,7 +10,6 @@ import Control.Monad (when)
 
 import Foreign
 import Foreign.C.Types
-import Foreign.Marshal.Array (advancePtr)
 
 root :: Rank
 root = 0
@@ -41,7 +40,7 @@ syncSendRecvTest  :: (Comm -> Rank -> Tag -> ArrMsg -> IO ()) -> Rank -> IO ()
 asyncSendRecvTest :: (Comm -> Rank -> Tag -> ArrMsg -> IO Request) -> Rank -> IO ()
 rsendRecvTest, broadcastTest, scatterTest, scattervTest, gatherTest, gathervTest :: Rank -> IO ()
 allgatherTest, allgathervTest, alltoallTest, alltoallvTest, reduceTest, allreduceTest, reduceScatterTest :: Rank -> IO ()
-reduceUserOpTest :: Rank -> IO ()
+reduceUserOpTest, asyncSendRecvWaitallTest :: Rank -> IO ()
 
 -- StorableArray tests
 type ArrMsg = StorableArray Int Int
